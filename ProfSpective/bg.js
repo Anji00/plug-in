05741da62d9f3dd3fc3d5965030b2875ad272069
faizachy <faizachy18@gmail.com>
2024-01-27 18:43:11 -0500

    content-script.js

--- ProfSpective/bg.js
+++ ProfSpective/bg.js
@@ -1,22 +1,20 @@
 // run in the background
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("https://*.mcgill.ca/study/*/courses/*")) {
+    if (tab.url && tab.url.includes("mcgill.ca/study/")) {
         
         // for storage?? do we need?
-        const queryParams = "courses/"[1];
+        const queryParams = tab.url.split("courses/")[1];
 
         const urlParams = new URLSearchParams(queryParams);
-        console.log(hi);
+        console.log(urlParams);
+
         chrome.tabs.sendMessage(tabId, {
             type: "NEW",
             courseCode: urlParams.get("v"),
         });
 
     }
-}
-
-
-)
+});
 
 
--- ProfSpective/content_script.js
+++ ProfSpective/content_script.js
@@ -1,25 +1,78 @@
 // will run in the context of the webpage 
 
-
 (() => {
     let courseInstructors;
     let currentCourse = "";
+    //const Papa = require('papaparse');
+
+
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, courseCode} = obj;
-    if (type == NEW) {
+    const { type, value, courseCode} = obj;
+    if (type === "NEW") {
         currentCourse = courseCode;
+        console.log(error);
         newCourseLoaded();
-
     }
+});
 
-    const newCourseLoaded = async() => {
+    const newCourseLoaded = async () => {
 
-        const infoLoaded = document.getElementsByClassName("prof-info")[0];
-        if (!infoLoaded) {
+        // const infoLoaded = document.getElementsByClassName("prof-info")[0];
+
+        // if (!infoLoaded)
+         
             courseInstructors = document.getElementsByClassName("catalog-instructors")[0];
+            
+            console.log(courseInstructors);
+            const courseInstructors2 = courseInstructors.textContent.trim();
+
+            // Extract the instructor names using a regular expression
+            const instructorNames = courseInstructors2.match(/([A-Za-z]+,\s[A-Za-z]+)\s\([A-Za-z]+\)/g);
+            
+            if (instructorNames) {
+                // Log or process the extracted instructor names
+                console.log("Instructor Names:", instructorNames);
+            } else {
+                console.log("No instructor names found in the given format.");
+            
+            }
+            
+            /** 
+            const csvPath = 'resources/instructors.csv';
+            Papa.parse(csvPath, {
+                download: true,
+                header: true,
+                dynamicTyping: true,
+                complete: function (results) {
+                const instructorsData = results.data;
+                for (const instructorData of instructorsData) {
+                    const name = instructorData.Name;
+                    const rating = instructorData.Rating;
+                }
+            }
+            
+            });
+
+            */
+
+
+            /** 
+            const targetProfessorName = "John Doe";  // Replace with the professor's name you're searching for
+
+            for (const instructorData of instructorsData) {
+            const instructorName = instructorData.Name;
+
+            if (instructorName === targetProfessorName) {
+            // Found the desired professor
+            console.log("Professor found:", instructorData);
+             break;  // Assuming you only need to find one match
+            }
+            }
+
+            */
+
             
         }
         
-    }
-});
-})();
+    
+    })();
