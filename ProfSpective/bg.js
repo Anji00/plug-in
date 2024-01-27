227c33f7124d50daeb034c42a2a42af316b85cce
faizachy <faizachy18@gmail.com>
2024-01-27 16:05:47 -0500

    content_script.js

--- ProfSpective/bg.js
+++ ProfSpective/bg.js
@@ -1,6 +1,20 @@
 // run in the background
 
-chrome.tabs.onUpdated.addListener(
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if (tab.url && tab.url.includes("https://*.mcgill.ca/study/*/courses/*")) {
+        
+        // for storage?? do we need?
+        const queryParams = "courses/"[1];
+
+        const urlParams = new URLSearchParams(queryParams);
+        console.log(hi);
+        chrome.tabs.sendMessage(tabId, {
+            type: "NEW",
+            courseCode: urlParams.get("v"),
+        });
+
+    }
+}
 
 
 )
--- ProfSpective/content_script.js
+++ ProfSpective/content_script.js
@@ -1,1 +1,25 @@
 // will run in the context of the webpage 
+
+
+(() => {
+    let courseInstructors;
+    let currentCourse = "";
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, courseCode} = obj;
+    if (type == NEW) {
+        currentCourse = courseCode;
+        newCourseLoaded();
+
+    }
+
+    const newCourseLoaded = async() => {
+
+        const infoLoaded = document.getElementsByClassName("prof-info")[0];
+        if (!infoLoaded) {
+            courseInstructors = document.getElementsByClassName("catalog-instructors")[0];
+            
+        }
+        
+    }
+});
+})();
